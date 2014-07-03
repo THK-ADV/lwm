a6524ad7af9728501be904a7cf87ab48e4162b5a
Robert Giacinto <robert.giacinto@gmail.com>
2014-07-03 14:59:32 +0200

    added signIn function and getSession function with placeholder implementation.

--- app/controllers/SessionManagement.scala
+++ app/controllers/SessionManagement.scala
@@ -1,10 +1,36 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.mvc._
+
+
+case class Session(id: String)
+
+
+
 
 /**
  * Session Management.
  */
 object SessionManagement extends Controller{
+  import util.LDAPAuthentication._
+
+  private var sessions: Set[Session] = Set.empty
+
+  def signIn(user: String, password: String) = Action{
+    authenticate(user, password) match{
+      case Left(message) =>
+        Unauthorized(message)
+      case Right(b) =>
+        val session = getSession(user)
+        sessions += session
+        Ok(session.id)
+    }
+  }
+
+  def getSession(user: String): Session = {
+    val sessionID = s"${user.hashCode}::${System.nanoTime()}"
+    Session(sessionID)
+  }
+
 
 }
