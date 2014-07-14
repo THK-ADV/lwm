542ead569f3091ef16cf1da132dd59c8f75c1b80
Robert Giacinto <robert.giacinto@gmail.com>
2014-07-15 01:05:02 +0200

    Using actor for session handling. Makes sessions available to all backend services without the need to use the rest api or have a synchronised object

--- app/controllers/SessionManagement.scala
+++ app/controllers/SessionManagement.scala
@@ -1,16 +1,19 @@
 package controllers
 
-import com.typesafe.config.ConfigFactory
+import akka.actor.{Props, Actor}
+import akka.actor.Actor.Receive
+import akka.util.Timeout
+import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.commons.codec.digest.DigestUtils
 import org.joda.time.DateTime
-import play.api.libs.concurrent.Promise
+import play.Configuration
+import play.api.libs.concurrent.{Akka, Promise}
 import play.api.mvc.{Action, Controller, Security}
+import util.LDAPAuthentication._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Right, Left}
-
-
-case class Session(id: String, expirationDate: DateTime)
+import play.api.Play.current
 
 
 /**
@@ -18,36 +21,35 @@ case class Session(id: String, expirationDate: DateTime)
  */
 object SessionManagement extends Controller {
 
-  import util.LDAPAuthentication._
   import scala.concurrent.duration._
+  import akka.pattern.ask
+  import scala.concurrent.ExecutionContext.Implicits.global
 
-  private var sessions: Set[Session] = Set.empty
+
   private val config = play.Configuration.root()
+  private implicit val timeout = Timeout(15.seconds)
 
- /*
-  * {
-  *   user: "bla",
-  *   password: "hallo123"
-  * }
-  */
+  private val sessionsHandler = Akka.system.actorOf(SessionHandler.props(config), "sessions")
+
+  /*
+   * {
+   *   user: "bla",
+   *   password: "hallo123"
+   * }
+   */
   def login() = Action.async(parse.json) { request =>
+
     val user = (request.body \ "user").as[String]
     val password = (request.body \ "password").as[String]
+
     val timeoutFuture = Promise.timeout("Oops", 15.second)
+    val authFuture = (sessionsHandler ? SessionHandler.AuthenticationRequest(user, password)).mapTo[Either[String, SessionHandler.Session]]
 
-
-   val DN = config.getString("lwm.bindDN")
-   val bindHost = config.getString("lwm.bindHost")
-   val bindPort = config.getInt("lwm.bindPort")
-
-    val authFuture = authenticate(user, password, bindHost, bindPort, DN)
 
     Future.firstCompletedOf(Seq(authFuture, timeoutFuture)).map {
       case Left(message: String) =>
         Unauthorized(message)
-      case Right(b: Boolean) =>
-        val session = createSessionID(user)
-        sessions += session
+      case Right(session: SessionHandler.Session) =>
         Ok("Login successful").withSession(
           Security.username -> "user",
           "session" -> session.id,
@@ -58,28 +60,57 @@ object SessionManagement extends Controller {
   }
 
   def logout() = Action { request =>
-    val id = request.session.get("session")
+    request.session.get("session").map(sessionsHandler ! SessionHandler.LogoutRequest(_))
 
-    id match {
-      case Some(sid) =>
-        if (sessions.exists(_.id == sid)) {
-          sessions -= sessions.find(_.id == sid).get
-          NoContent.withNewSession
-        } else {
-          NoContent.withNewSession
-        }
-      case None =>
-        NoContent.withNewSession
-    }
+    NoContent.withNewSession
+  }
+
+
+}
+
+
+object SessionHandler {
+
+  case class AuthenticationRequest(user: String, password: String)
+
+  case class LogoutRequest(sessionID: String)
+
+  case class Session(id: String, expirationDate: DateTime)
+
+  def props(config: Configuration) = Props(new SessionHandler(config))
+}
+
+class SessionHandler(config: Configuration) extends Actor {
+
+  import SessionHandler._
+
+  private var sessions: Set[Session] = Set.empty
+
+  val DN = config.getString("lwm.bindDN")
+  val bindHost = config.getString("lwm.bindHost")
+  val bindPort = config.getInt("lwm.bindPort")
+
+  def receive: Receive = {
+    case AuthenticationRequest(user, password) =>
+      val authFuture = authenticate(user, password, bindHost, bindPort, DN)
+      val requester = sender()
+      authFuture.map{
+        case l @ Left(error) =>
+          requester ! l
+        case Right(success) =>
+          val session =  createSessionID(user)
+          sessions += session
+          requester ! Right(session)
+      }
+    case LogoutRequest(sessionID) =>
+      sessions.find(_.id == sessionID).map(sessions -= _)
   }
 
   private def createSessionID(user: String): Session = {
     val sessionID = DigestUtils.sha1Hex(s"$user::${System.nanoTime()}")
     val lifetime = config.getInt("lwm.sessions.lifetime", 8)
-    val expirationDate =  DateTime.now().plusHours(lifetime)
+    val expirationDate = DateTime.now().plusHours(lifetime)
 
     Session(sessionID, expirationDate)
   }
-
-
 }
