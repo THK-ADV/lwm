181867981648549a37921b62c4fd19f085567ec4
Robert Giacinto <robert.giacinto@gmail.com>
2014-09-13 17:53:21 +0200

    labwork assignment stuff

--- app/controllers/LabworkManagementController.scala
+++ app/controllers/LabworkManagementController.scala
@@ -2,58 +2,58 @@ package controllers
 
 import controllers.StudentsManagement._
 import models._
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{ Action, Controller }
 import utils.Security.Authentication
-import utils.semantic.Vocabulary.{OWL, RDFS, LWM}
-import utils.semantic.{RDFNode, Individual, Resource}
+import utils.semantic.Vocabulary.{ OWL, RDFS, LWM }
+import utils.semantic.{ RDFNode, Individual, Resource }
 import utils.Global._
 import scala.concurrent.Future
 
 /**
- * Created by rgiacinto on 20/08/14.
- */
+  * Created by rgiacinto on 20/08/14.
+  */
 object LabworkManagementController extends Controller with Authentication {
 
   import scala.concurrent.ExecutionContext.Implicits.global
   import scala.concurrent.duration._
 
-  def index() = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session =>
-    Action.async { request =>
+  def index() = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session ⇒
+    Action.async { request ⇒
       for {
-        courses <- Courses.all()
-        degrees <- Degrees.all()
-        labworks <- LabWorks.all()
+        courses ← Courses.all()
+        degrees ← Degrees.all()
+        labworks ← LabWorks.all()
+        semesters ← Semesters.all()
       } yield {
-        Ok(views.html.labwork_management(labworks.toList, degrees.toList, courses.toList, LabWorkForms.labworkForm))
+        Ok(views.html.labwork_management(semesters.toList, labworks.toList, degrees.toList, courses.toList, LabWorkForms.labworkForm))
       }
     }
   }
 
-
-//TODO: ADD ASSIGNMENTS
-  def edit(id: String) = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session =>
-    Action.async { request =>
-        val labworkIndividual = Individual(Resource(id))
-        val groups = labworkIndividual.props.getOrElse(LWM.hasGroup, List.empty[Resource]).map(r => Individual(r.asResource().get))
-        Future.successful(Ok(views.html.labWorkInformation(labworkIndividual, groups, Nil)))
+  //TODO: ADD ASSIGNMENTS
+  def edit(id: String) = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session ⇒
+    Action.async { request ⇒
+      val labworkIndividual = Individual(Resource(id))
+      val groups = labworkIndividual.props.getOrElse(LWM.hasGroup, List.empty[Resource]).map(r ⇒ Individual(r.asResource().get))
+      Future.successful(Ok(views.html.labWorkInformation(labworkIndividual, groups, Nil)))
     }
   }
 
-
-  def labWorkPost() = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session =>
-    Action.async { implicit request =>
+  def labWorkPost() = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session ⇒
+    Action.async { implicit request ⇒
       LabWorkForms.labworkForm.bindFromRequest.fold(
-        formWithErrors => {
+        formWithErrors ⇒ {
           for {
-            labworks <- LabWorks.all()
-            courses <- Courses.all()
-            degrees <- Degrees.all()
+            labworks ← LabWorks.all()
+            courses ← Courses.all()
+            degrees ← Degrees.all()
+            semesters ← Semesters.all()
           } yield {
-            BadRequest(views.html.labwork_management(labworks.toList, degrees.toList, courses.toList, formWithErrors))
+            BadRequest(views.html.labwork_management(semesters.toList, labworks.toList, degrees.toList, courses.toList, formWithErrors))
           }
         },
-        labwork => {
-          LabWorks.create(labwork).map{ _ =>
+        labwork ⇒ {
+          LabWorks.create(labwork).map { _ ⇒
             Redirect(routes.LabworkManagementController.index())
           }
         }
@@ -61,16 +61,11 @@ object LabworkManagementController extends Controller with Authentication {
     }
   }
 
-  def labworkRemoval = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session =>
-    Action.async(parse.json) { implicit request =>
-      for {
-        labworks <- LabWorks.all()
-        courses <- Courses.all()
-        degrees <- Degrees.all()
-      } yield {
-        val id = (request.body \ "id").as[String]
-        LabWorks.delete(Resource(id))
-        Ok(views.html.labwork_management(labworks.toList, degrees.toList, courses.toList, LabWorkForms.labworkForm))
+  def labworkRemoval = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session ⇒
+    Action.async(parse.json) { implicit request ⇒
+      val id = (request.body \ "id").as[String]
+      LabWorks.delete(Resource(id)).map { _ ⇒
+        Redirect(routes.LabworkManagementController.index())
       }
     }
   }
