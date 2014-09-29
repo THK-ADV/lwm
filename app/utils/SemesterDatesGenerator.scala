c95d24046f27b3a8d7bc91446d7d2015eb381e45
Robert Giacinto <robert.giacinto@gmail.com>
2014-09-29 10:59:03 +0200

    only add schedule dates when there are enough dates availables

--- app/utils/SemesterDatesGenerator.scala
+++ app/utils/SemesterDatesGenerator.scala
@@ -52,56 +52,56 @@ object SemesterDatesGenerator {
         if (possibleDates.size < groupCount * assignmentCount) {
           println(s"ERROR: Not enough available assignment dates for $groupCount groups with $assignmentCount assignments")
           println(possibleDates)
-        }
+        }else{
+          val orderedAssocs = (for {
+            assocNode ← labwork.props.getOrElse(LWM.hasAssignmentAssociation, Nil)
+            assoc = assocNode.asResource().get
+            orderIdNode ← Individual(assoc).props.getOrElse(LWM.hasOrderId, Nil)
+            orderId = orderIdNode.asLiteral().get
+            prepTime = Individual(assoc).props.getOrElse(LWM.hasPreparationTime, Nil).headOption.getOrElse(StringLiteral("0")).value.toInt
+          } yield orderId.decodedString.toInt -> (assoc, prepTime.toInt)).sortBy(_._1)
 
-        val orderedAssocs = (for {
-          assocNode ← labwork.props.getOrElse(LWM.hasAssignmentAssociation, Nil)
-          assoc = assocNode.asResource().get
-          orderIdNode ← Individual(assoc).props.getOrElse(LWM.hasOrderId, Nil)
-          orderId = orderIdNode.asLiteral().get
-          prepTime = Individual(assoc).props.getOrElse(LWM.hasPreparationTime, Nil).headOption.getOrElse(StringLiteral("0")).value.toInt
-        } yield orderId.decodedString.toInt -> (assoc, prepTime.toInt)).sortBy(_._1)
+          val orderedGroups = (for {
+            groupNode ← labwork.props.getOrElse(LWM.hasGroup, Nil)
+            group = groupNode.asResource().get
+            groupIdNode ← Individual(group).props.getOrElse(LWM.hasGroupId, Nil)
+            orderId = groupIdNode.asLiteral().get
+          } yield orderId.decodedString -> group).sortBy(_._1)
 
-        val orderedGroups = (for {
-          groupNode ← labwork.props.getOrElse(LWM.hasGroup, Nil)
-          group = groupNode.asResource().get
-          groupIdNode ← Individual(group).props.getOrElse(LWM.hasGroupId, Nil)
-          orderId = groupIdNode.asLiteral().get
-        } yield orderId.decodedString -> group).sortBy(_._1)
-
-        val assignmentDates = for {
-          assoc ← orderedAssocs
-          group ← orderedGroups
-        } yield {
-          val date = possibleDates.head
-          possibleDates = possibleDates.tail
-          AssignmentDateAssociation(group, assoc, date)
-        }
-
-        val assignmentsPerGroup = assignmentDates.groupBy(_.group._2).map { group ⇒
-          group._1 -> group._2.sortWith((a, b) ⇒ a.date.date.compareTo(b.date.date) < 0)
-        }
-
-        val dues = assignmentsPerGroup.map { entry ⇒
-          var dates = entry._2
-          var d = List[DueDateAssociation]()
-          while (dates.size > 0) {
-            val current = dates.head
-            val prepTime = current.association._2._2
-            val dueDate = dates.drop(prepTime).head
-            dates = dates.tail
-            d = DueDateAssociation(current.group._2, current.association._2._1, dueDate.date) :: d
+          val assignmentDates = for {
+            assoc ← orderedAssocs
+            group ← orderedGroups
+          } yield {
+            val date = possibleDates.head
+            possibleDates = possibleDates.tail
+            AssignmentDateAssociation(group, assoc, date)
           }
-          d
-        }.flatten.groupBy(e ⇒ (e.association, e.group))
 
-        val schedule = assignmentDates.map { date ⇒
-          val due = dues((date.association._2._1, date.group._2)).head
-          ScheduleAssociation(date.group._2, date.association._2._1, date.date.date, due.date.date, date.date.entry.ownResource.get, due.date.entry.ownResource.get)
-        }
+          val assignmentsPerGroup = assignmentDates.groupBy(_.group._2).map { group ⇒
+            group._1 -> group._2.sortWith((a, b) ⇒ a.date.date.compareTo(b.date.date) < 0)
+          }
 
-        schedule.map { s ⇒
-          ScheduleAssociations.create(s)
+          val dues = assignmentsPerGroup.map { entry ⇒
+            var dates = entry._2
+            var d = List[DueDateAssociation]()
+            while (dates.size > 0) {
+              val current = dates.head
+              val prepTime = current.association._2._2
+              val dueDate = dates.drop(prepTime).head
+              dates = dates.tail
+              d = DueDateAssociation(current.group._2, current.association._2._1, dueDate.date) :: d
+            }
+            d
+          }.flatten.groupBy(e ⇒ (e.association, e.group))
+
+          val schedule = assignmentDates.map { date ⇒
+            val due = dues((date.association._2._1, date.group._2)).head
+            ScheduleAssociation(date.group._2, date.association._2._1, date.date.date, due.date.date, date.date.entry.ownResource.get, due.date.entry.ownResource.get)
+          }
+
+          schedule.map { s ⇒
+            ScheduleAssociations.create(s)
+          }
         }
 
       }
