d6cc7bda5e3ae4fa26a00c251ee806b4075cc159
AvramRobert <am11.robert@yahoo.com>
2014-11-17 18:07:26 +0100

    Arrows added

--- app/controllers/AdministrationDashboardController.scala
+++ app/controllers/AdministrationDashboardController.scala
@@ -13,11 +13,15 @@ import utils.Security.Authentication
 
 import scala.concurrent.Future
 
+object DefaultBounds {
+  val min = "-1"
+  val max = "7"
+}
 object AdministrationDashboardController extends Controller with Authentication {
 
-  def dashboard(count: String) = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session ⇒
+  def dashboard(lowerBound: String, upperBound: String) = hasPermissions(Permissions.AdminRole.permissions.toList: _*) { session ⇒
     Action.async { implicit request ⇒
-      Future.successful(Ok(views.html.dashboard_admin(LocalDate.now(), count.toInt)))
+      Future.successful(Ok(views.html.dashboard_admin(LocalDate.now(), lowerBound.toInt, upperBound.toInt)))
     }
   }
 
